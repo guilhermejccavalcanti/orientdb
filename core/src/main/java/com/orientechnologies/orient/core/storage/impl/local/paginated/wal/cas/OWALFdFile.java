83cb71d6f92472119c3ddf959495e4172c755046
laa <lomakin.andrey@gmail.com>
2018-08-01 16:23:54 +0300

    Flush out only last chunk of WAL file

--- core/src/main/java/com/orientechnologies/orient/core/storage/impl/local/paginated/wal/cas/OWALFdFile.java
+++ core/src/main/java/com/orientechnologies/orient/core/storage/impl/local/paginated/wal/cas/OWALFdFile.java
@@ -37,7 +37,8 @@ public class OWALFdFile implements OWALFile {
       buffer.position(buffer.position() + written);
 
       if (expectedInitialLen + written > 0 && ((expectedInitialLen + written) & (128 * 1024 * 1024 - 1)) == 0) {
-        ONative.instance().posix_fadvise(fd, 0, expectedInitialLen + written, ONative.POSIX_FADV_DONTNEED);
+        ONative.instance().posix_fadvise(fd, expectedInitialLen + written - 128 * 1024 * 1024, expectedInitialLen + written,
+            ONative.POSIX_FADV_DONTNEED);
       }
       return written;
     } catch (LastErrorException e) {
